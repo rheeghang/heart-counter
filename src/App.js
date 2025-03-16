72aaed973640ccc6a6f57104e790a495bf4ed806
“rheeghang <ryujh21h@pati.kr>
2025-03-16 22:37:52 +0900

    🎉 First commit - Heart Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0); // 숫자 상태 관리
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ 
+      textAlign: "center", marginTop: "50px"}}>
+        <title>counter </title>
+      <h1 
+        style={{
+          fontSize: "150px"
+        }}
+      >
+        {count}
+      </h1>
+      <button 
+        onClick={() => setCount(count + 1)}
+        style={{
+          fontSize: "150px",
+          marginBottom: "0px",
+          padding: "10px 20px",
+          border: "solid black 3px",
+          width: "60ew",
+          cursor: "pointer",
+          background: "white"
+        }}
+      >
+        ❤︎
+      </button>
     </div>
   );
 }
